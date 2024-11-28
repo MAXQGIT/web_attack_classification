0a63f37f1ec9b477fb030cf893d30a2523e37e20
MAXQGIT <83851714+MAXQGIT@users.noreply.github.com>
2024-11-28 17:25:46 +0800

    Add files via upload

--- self_model.py
+++ self_model.py
@@ -1,19 +1,24 @@
 import torch.nn as nn
 from Config import Config
 
+
 class LSTM_MODEL(nn.Module):
     def __init__(self, cfg):
         super(LSTM_MODEL, self).__init__()
         self.lstm = nn.LSTM(cfg.input_dim, cfg.hidden, batch_first=True)
         self.attention = nn.MultiheadAttention(cfg.hidden, num_heads=8)
+        self.dropout = nn.Dropout(0.3)
+        self.relu = nn.ReLU()
         self.gru = nn.GRU(cfg.hidden, cfg.hidden, batch_first=True)
         self.linear = nn.Linear(cfg.hidden, cfg.output_dim)
 
     def forward(self, x):
         x = x.reshape(x.shape[0], 1, -1)
         x, _ = self.lstm(x)
+        x = self.dropout(x)
         # y, attn_output_weights = self.attention(x, x, x)
         # y = x + y
+        x = self.relu(x)
         y, _ = self.gru(x)
         # y = x + y
         y = self.linear(y)
